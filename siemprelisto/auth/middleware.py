0fb50fa06258045b4ce5e45e8de2c21a9fb3a201
Yonatan Romero <romeroyonatan@gmail.com>
2018-02-12 21:59:45 -0300

    Refactor

--- siemprelisto/auth/middleware.py
+++ siemprelisto/auth/middleware.py
@@ -13,10 +13,23 @@ class AuthenticationMiddleware(object):
         Authorization: Bearer <token>
 
 
-        If resource not require authentication, set `auth_required`
-        attribute to False
+        If resource not require authentication, set `auth` dict with method
+        to False (Its true by default.)
+
+        ```
+        class Resource:
+            auth = {'get': False}
+            def on_get(self, req, resp):
+                'Dont need auth.'
+                pass
+
+            def on_post(self, req, resp):
+                'Need auth.'
+                pass
+        ```
         '''
-        if getattr(resource, 'auth_required', True):
+        auth_map = getattr(resource, 'auth', {})
+        if auth_map.get(req.method, True):
             authorization_header = req.auth
             # check if authorizacion header exists
             if not authorization_header:
@@ -28,7 +41,7 @@ class AuthenticationMiddleware(object):
 
             # check if is valid token
             token = authorization_header.split()[-1]
-            if not utils.is_valid_token(token):
-                raise falcon.HTTPForbidden()
-            else:
+            if utils.is_valid_token(token):
                 req.user = utils.get_user(token)
+            else:
+                raise falcon.HTTPForbidden()
